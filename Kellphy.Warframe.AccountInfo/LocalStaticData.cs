5e2bb452f8a91dc2a33912baf8e4d6e3a6c154ad
Kellphy <virtual_44@yahoo.com>
2024-01-12 01:31:20 +0200

    Moved LocalStaticData

---
+++ Kellphy.Warframe.AccountInfo/LocalStaticData.cs
@@ -0,0 +1,15 @@
+﻿using Kellphy.Warframe.AccountInfo.Models;
+
+namespace Kellphy.Warframe.AccountInfo
+{
+	public static class LocalStaticData
+	{
+		public static List<RelicItem>? relics;
+
+		public static void Log(string type, string text)
+		{
+			Console.WriteLine(text);
+		}
+
+	}
+}
--- Kellphy.Warframe.AccountInfo/Program.cs
+++ Kellphy.Warframe.AccountInfo/Program.cs
@@ -245,15 +245,3 @@ namespace Kellphy.Warframe.AccountInfo
 
 	}
 }
-
-
-public static class LocalStaticData
-{
-	public static List<RelicItem>? relics;
-
-	public static void Log(string type, string text)
-	{
-		Console.WriteLine(text);
-	}
-
-}
